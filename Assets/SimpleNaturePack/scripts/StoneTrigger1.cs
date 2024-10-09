038bd4a38d11998d049f9245c84c5e2b44506e66
Awu_Lin <94472422+Awu-Lin@users.noreply.github.com>
2024-10-09 10:32:39 -0700

    Implemented sound playback functionality, modified some comments

--- Assets/SimpleNaturePack/scripts/StoneTrigger1.cs
+++ Assets/SimpleNaturePack/scripts/StoneTrigger1.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class StoneTrigger1 : MonoBehaviour
 {
-    public GameObject[] trees; // Drag and drop tree1, tree2, tree3 (three objecy trees)
+    public GameObject[] trees; // Drag and drop tree1, tree2, tree3 (three object trees)
+    public Transform cameraOffset; // Camera Offset for transmission
+    public Vector3 teleportPosition = new Vector3(44.17757f, 4.83053f, -3.58039f); // Teleport position
+    public AudioSource audioSource; // Audio source for playing music
 
     void OnTriggerEnter(Collider other)
     {
         // Detect if the incoming object is a blue stone tagged 'disappearance'
         if (other.CompareTag("disappearance"))
         {
-            // Randomly select a tree
+            // Randomly select a tree and make it disappear
             int randomTreeIndex = Random.Range(0, trees.Length);
-            trees[randomTreeIndex].SetActive(false); // make it Boo! Gone
+            trees[randomTreeIndex].SetActive(false);
+        }
+
+        // Detect if the incoming object is a green stone tagged 'transmission'
+        if (other.CompareTag("transmission"))
+        {
+            // Teleport the Camera Offset to the specified position
+            cameraOffset.position = teleportPosition;
+        }
+
+        // Detect if the incoming object is a red stone tagged 'audible'
+        if (other.CompareTag("audible"))
+        {
+            // Play the audio when red stone touches the target
+            if (audioSource != null && !audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
     }
 }
