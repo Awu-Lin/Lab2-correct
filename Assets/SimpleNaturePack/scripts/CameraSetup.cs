ec2bea19c16bff8e993980d9227d470de5294606
Awu_Lin <94472422+Awu-Lin@users.noreply.github.com>
2024-10-09 12:20:41 -0700

    change

--- Assets/SimpleNaturePack/scripts/CameraSetup.cs
+++ Assets/SimpleNaturePack/scripts/CameraSetup.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSetup : MonoBehaviour
-{
+{   //Parameters for setting the initial camera position
     public float positionX = -3.5f;
     public float positionY = 0.4f;
     public float positionZ = -10.1f;
 
     void Start()
     {
+        //Adjust the camera position to the set position
         transform.position = new Vector3(positionX, positionY, positionZ);
     }
 }
